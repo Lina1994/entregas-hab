87e12a580a65b2573dc5e56c03012a81840f2582
Lina1994 <luciagc_94@outlook.es>
2020-06-04 23:22:04 +0200

    CV mas info img border-radius random change every 2s

---
+++ css/Ejercicio-3-CV/www/JS/main.js
@@ -0,0 +1,9 @@
+
+const rn = (max) => Math.floor(Math.random() * max +20);
+const borderR = document.querySelector("img.second");
+console.log(borderR);
+function changeradius() {
+    borderR.style.borderRadius = `${rn(100)}% ${rn(80)}% ${rn(80)}% ${rn(80)}%`;
+    //console.log(borderR.style.borderRadius);
+}
+const interval = setInterval(changeradius, 2000); 
